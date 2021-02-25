910fb87a90888fa10d815622b1579f3c8fc116f1
ranvuemor <amishkushwaha7@gmail.com>
2021-02-25 19:42:18 +0100

    first commit /amishk

--- main.cpp
+++ main.cpp
@@ -5,22 +5,14 @@
 #include "help.h"
 #include "aboutUs.h"
 #include "newGame.h"
+#include "header.h"
 
 using namespace std;
 
-void gotoxy(int x,int y){
-    //cout << 0x1B << y << x ;
-    printf("%c[%d;%df",0x1B,y,x);
+int main(){
+    header();
+
 }
-void clrscr(void){
-    system("clear");
-}
-int main(){    
-    int x=50, y=10;
-    clrscr();
-    gotoxy(x,y);
-    cout << "Typing Game" << endl;
-    //gotoxy(x,y);
-    cout << "Today's Date" << endl;
-    return 0;
-}
+
+
+
