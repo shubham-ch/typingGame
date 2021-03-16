ebc218fa3a49e06d8cad41509e6be2efd7603181
shubham-ch <shubhamchaurasia551@gmail.com>
2021-03-16 20:46:47 +0100

    error correction

--- newGame.h
+++ newGame.h
@@ -4,26 +4,27 @@
 
 using namespace std;
 
-void timeDuration(int time){
-    int time_spent;
-    clock_t begin;
-    time_spent = (int)(clock()-begin)/CLOCKS_PER_SEC;
-    if (time_spent >= time){
-        storeScore();
-        printScore();
-        break;
-    }
-}
+// void timeDuration(int time){
+//     int time_spent;
+//     clock_t begin;
+//     begin = clock();
+//     time_spent = (int)(clock()-begin)/CLOCKS_PER_SEC;
+//     if (time_spent >= time){
+//         // storeScore(score, speed, level);
+//         printScore();
+//     }
+// }
 
-void level(int mode){
+void level(int mode, int points){
     //header();
-    int rtime, n; // n is random length for a word // rtime is timeduration for which game would run
+    int rtime, time_spent, n; // n is random length for a word // rtime is timeduration for which game would run
     cout << "Enter the number of minutes you wanna play the game" << endl;
-    cin >> rtime >> endl;
-    while(1){
-        timeDuration(rtime);
+    cin >> rtime;
+    clock_t begin;
+    begin = clock();
+    do{
         int r[9];
-        char compare;
+        char compare[9];
         n = rand()%100;
         n = n % 10;
         cout << "Type the following code" << endl;
@@ -33,33 +34,32 @@ void level(int mode){
             cout << (char)r[i] ;
         }
         cout << endl;
-        int j=0;
-        do{
-            cin >> compare;
-            j++;
-        }while((compare == r[j]) && (j < n));
-    }
+        int j=0, points = 0;
+        for(j = 0; j < n; j++){
+            cin >> compare[j];
+        }
+    }while(time_spent = (int)(clock()-begin)/CLOCKS_PER_SEC);
     
    
 }
 
 void newGame() {
-    int bool=0;
+    int bool1=0;
     //header();
     cout << "Please enter your difficulty level" << endl;
     cout << "1. Easy" << endl;
     cout << "2. Medium" << endl;
     cout << "3. Hard" << endl;
     cout << "4. Back to main menu" << endl;
-    int temp;
+    int temp, points;
     cin >> temp >> endl;
     do{
         switch (temp) {
-        case 1: level(65); break;
-        case 2: level(97); break;
-        case 3: level(120); break;
-        case 4: main(); break;
-        default: bool=1;
+        case 1: level(65, points); break;
+        case 2: level(97, points); break;
+        case 3: level(120, points); break;
+        //case 4: main(); break;
+        default: bool1=1;
         }
-    }while(bool=1)
+    }while(bool1==1);
 }
